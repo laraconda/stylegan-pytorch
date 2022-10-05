83d00e94652499e265e00a0e4b075e4e5d82daa3
Cesar Lara <larachicharo@protonmail.com>
2022-10-05 15:48:22 -0500

    Documenting networks.py and blocks.py inside models/generator/

--- src/models/generator/blocks.py
+++ src/models/generator/blocks.py
@@ -6,13 +6,52 @@ from src.device import device
 
 
 class AdaIn(nn.Module):
+    """
+    AdaIn layer. Performs the AdaIN operation on a tensor input
+
+    Attributes
+    ----------
+    inorm: InstanceNorm2d
+        An instance normalization instance
+    scale: NormalLinear
+        A dense layer used to scale the input tensor
+    offset: NormalLinear
+        A dense layer used to offset the input tensor
+
+    Methods
+    -------
+    forward(x, w)
+        Performs the AdaIN operation on the x tensor based on the tensor w
+    """
     def __init__(self, channels=BASE_DIM):
+        """
+        Parameters
+        ----------
+        channels: int
+            Number of channels of this instance
+        """
         super(AdaIn, self).__init__()
         self.inorm = nn.InstanceNorm2d(channels)
         self.scale = NormalLinear(BASE_DIM, channels)
         self.offset = NormalLinear(BASE_DIM, channels)
 
     def forward(self, x, w):
+        """
+        Performs the AdaIN operation on the x tensor based on the tensor w
+
+        Parameters
+        ----------
+        x: tensor
+            The tensor to be transformed
+        w: tensor
+            The 'style' tensor where the variables scale and offset
+            come from after two different linear transformations on w
+
+        Returns
+        -------
+        tensor
+            The result of the transformation
+        """
         normalizedx = self.inorm(x)
         scale = self.scale(w)
         offset = self.offset(w)
@@ -22,19 +61,90 @@ class AdaIn(nn.Module):
 
 
 class Noise(nn.Module):
+    """
+    Noise layer. Injects random noise into an input tensor
+
+    Attributes
+    ----------
+    weight: Parameter
+        A learnable parameter used to scale a random tensor
+    resolution: int
+        Resolution of the image tensors the layer is going to process
+
+    Methods
+    -------
+    forward(x)
+        Injects random noise into the input tensor x
+    """
     def __init__(self, resolution, channels=BASE_DIM, *args, **kwargs):
+        """
+        Parameters
+        ----------
+        resolution: int
+            The image resolution on which this instance is going to operate
+        channels: int
+            Number of channels of this instance
+        """
         super(Noise, self).__init__(*args, **kwargs)
         self.weight = nn.Parameter(torch.randn((1, channels, 1, 1)))
         self.resolution = resolution
 
     def forward(self, x):
+        """
+        Injects scales a random tensor and injects it into the input tensor x
+
+        Parameters
+        ----------
+        x: tensor
+            The tensor on which the transformation of injecting noise
+            is going to be applied
+
+        Returns
+        -------
+        tensor
+            The result of the transformation
+        """
         noise = torch.randn((x.shape[0], 1, self.resolution, self.resolution), device=device)
         return x + noise * self.weight
 
 
 class BasicSynthesisBlock(nn.Module):
-    def __init__(self, resolution, input_channels, output_channels, *args, **kwargs):
-        super(BasicSynthesisBlock, self).__init__(*args, **kwargs)
+    """
+    Defines a series of layers common for all synthesis blocks
+
+    Attributes
+    ----------
+        conv1: NormalConv2d
+            First convolutional layer
+        noise1: Noise
+            First noise layer
+        act: LeakyReLU
+            Activation. ReLU with a slope
+        adain1: AdaIn
+            First adain layer
+        conv2: NormalConv2d
+            Second convolution layer
+        noise2: Noise
+            Second noise layer
+        adain2: AdaIn
+            Second AdaIN layer
+    Methods
+    -------
+    forward(x, w)
+        Feeds forward the input tensor 'x' through the layers
+    """
+    def __init__(self, resolution, input_channels, output_channels):
+        """
+        Parameters
+        ----------
+        resolution: int
+            Resolution of the image tensors the block is going to process
+        input_channels: int
+            Number of input channels for this block
+        output_channels:
+            Number of output channels for this block
+        """
+        super(BasicSynthesisBlock, self).__init__()
         self.conv1 = NormalConv2d(input_channels, input_channels, 3, padding='same')
         self.noise1 = Noise(resolution, input_channels)
         self.act = nn.LeakyReLU(0.2)
@@ -44,6 +154,23 @@ class BasicSynthesisBlock(nn.Module):
         self.adain2 = AdaIn(output_channels)
 
     def forward(self, x, w):
+        """
+        Feeds forward the input tensor 'x' through the layers
+
+        Parameters
+        ----------
+        x: tensor
+            The tensor to be feed forwarded
+
+        w: tensor
+            The 'style' tensor. AdaIN transformations use it to transform
+            the x tensor
+
+        Returns
+        -------
+        tensor
+            The result of the transformations
+        """
         x = self.conv1(x)
         x = self.noise1(x)
         x = self.act(x)
@@ -56,7 +183,33 @@ class BasicSynthesisBlock(nn.Module):
 
 
 class FirstSynthesisBlock(nn.Module):
+    """
+    The first block of a synthesis network
+
+    Attributes
+    ----------
+    constant: Parameter
+        Learnable parameter. The very beginning of the block an thus of a network
+    basicblock: BasicSynthesisBlock
+        A basic synthesis block
+
+    Methods
+    -------
+    forward(w)
+        Given a style 'w', it returns a tensor after transforming the constant
+        using the basic block
+    """
     def __init__(self, resolution, channels=BASE_DIM):
+        """
+        Parameters
+        ----------
+        resolution: int
+            Resolution of the image tensors the block is going to process
+
+        channels: int
+            Number of channels of this block
+
+        """
         super(FirstSynthesisBlock, self).__init__()
         self.constant = nn.Parameter(
             torch.randn((1, channels, resolution, resolution))
@@ -64,18 +217,71 @@ class FirstSynthesisBlock(nn.Module):
         self.basicblock = BasicSynthesisBlock(resolution, channels, channels)
 
     def forward(self, w):
+        """
+        Given a style 'w', it returns a tensor after transforming the constant
+        using the basic block
+
+        Parameters
+        ----------
+        w: tensor
+            Style tensor
+
+        Returns
+        -------
+        tensor
+            The constant tensor transformed by the operations defined by the basic block
+        """
         x = self.constant
         x = self.basicblock(x, w)
         return x
 
 
 class SynthesisBlock(nn.Module):
+    """
+    Defines a synthesis block (other than the first one)
+
+    Attributes
+    ----------
+    upsample: Upsample
+        layer used to upsample a tensor input
+    basicblock: BasicSynthesisBlock
+        A basic sinthesis block
+
+    Methods
+    -------
+    forward(x, w)
+        Upsamples the tensor 'x' and transforms it
+    """
     def __init__(self, resolution, input_channels, output_channels):
+        """
+        Parameters
+        ----------
+        resolution: int
+            Resolution of the image tensors this block is going to process
+        input_channels: int
+            Number of input channels for this block
+        output_channels: int
+            Number of output channels for this block
+        """
         super(SynthesisBlock, self).__init__()
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
         self.basicblock = BasicSynthesisBlock(resolution, input_channels, output_channels)
 
     def forward(self, x, w):
+        """
+        Upsamples the tensor 'x' and transforms it based on 'w'
+
+        Parameters
+        ----------
+        x: tensor
+            An image tensor to be transformed
+        w: tensor
+            A style tensor
+
+        Returns
+        -------
+            The result of the transformations applied to 'x'
+        """
         x = self.upsample(x)
         x = self.basicblock(x, w)
         return x
--- src/models/generator/networks.py
+++ src/models/generator/networks.py
@@ -9,8 +9,22 @@ from src.settings import BASE_DIM
 
 
 class MappingNetwork(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(MappingNetwork, self).__init__(*args, **kwargs)
+    """
+    Mapping Network of the StyleGAN architecture.
+    Network that maps a z tensor into a w tensor
+
+    Attributes
+    ----------
+    fcs: Sequential
+        A sequence of dense layers
+
+    Methods
+    -------
+    forward(z)
+        Passes the 'z' tensor through the sequence 'fcs'
+    """
+    def __init__(self):
+        super(MappingNetwork, self).__init__()
         self.fcs = nn.Sequential(
             NormalLinear(BASE_DIM, BASE_DIM),
             nn.ReLU(),
@@ -18,23 +32,52 @@ class MappingNetwork(nn.Module):
             nn.ReLU()
         )
 
-    def forward(self, x):
-        return self.fcs(x)
+    def forward(self, z):
+        """
+        Passes the 'z' tensor through the sequence 'fcs'
+
+        Parameters
+        ----------
+        z: tensor
+            The tensor to be mapped
+
+        Returns
+        -------
+        tensor
+            The result of the mapping, a w tensor
+        """
+        return self.fcs(z)
 
 
 class SynthesisNetwork(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(SynthesisNetwork, self).__init__(*args, **kwargs)
-        self.conv_blocks = nn.ModuleList([                   # resolution, block_id
-            FirstSynthesisBlock(4),                            # 4,          0
-            SynthesisBlock(8, BASE_DIM, BASE_DIM),             # 8,          1
-            SynthesisBlock(16, BASE_DIM, BASE_DIM),            # 16,         2
-            SynthesisBlock(32, BASE_DIM, BASE_DIM),            # 32,         3
-            SynthesisBlock(64, BASE_DIM, BASE_DIM // 2),         # 64,         4
-            SynthesisBlock(128, BASE_DIM // 2, BASE_DIM // 4),     # 128,        5
-            SynthesisBlock(256, BASE_DIM // 4, BASE_DIM // 8),     # 256,        6
-            SynthesisBlock(512, BASE_DIM // 8, BASE_DIM // 16),    # 512,        7
-            SynthesisBlock(1024, BASE_DIM // 16, BASE_DIM // 32),  # 1024,       8
+    """
+    Synthesis Network of the StyleGAN architecture.
+    Progressively grows an image with the help of a set of w styles
+
+    Attributes
+    ----------
+    conv_blocks: ModuleList
+        A list of synthesis blocks
+    to_rgb: ModuleList
+        A list of ToRGB layers
+
+    Methods
+    -------
+    forward(z, w1, w2, alpha=1, stop_at_resolution=1024, style_mixing=False)
+        Produces an output image of resolution 'stop_at_resolution'
+    """
+    def __init__(self):
+        super(SynthesisNetwork, self).__init__()
+        self.conv_blocks = nn.ModuleList([                         # res,  block_id
+            FirstSynthesisBlock(4),                                # 4,    0
+            SynthesisBlock(8, BASE_DIM, BASE_DIM),                 # 8,    1
+            SynthesisBlock(16, BASE_DIM, BASE_DIM),                # 16,   2
+            SynthesisBlock(32, BASE_DIM, BASE_DIM),                # 32,   3
+            SynthesisBlock(64, BASE_DIM, BASE_DIM // 2),           # 64,   4
+            SynthesisBlock(128, BASE_DIM // 2, BASE_DIM // 4),     # 128,  5
+            SynthesisBlock(256, BASE_DIM // 4, BASE_DIM // 8),     # 256,  6
+            SynthesisBlock(512, BASE_DIM // 8, BASE_DIM // 16),    # 512,  7
+            SynthesisBlock(1024, BASE_DIM // 16, BASE_DIM // 32),  # 1024, 8
         ])
 
         self.to_rgb = nn.ModuleList([
@@ -49,25 +92,116 @@ class SynthesisNetwork(nn.Module):
             ToRGB(BASE_DIM // 32),
         ])
 
-    def upsample2x(self, image):
+    def _upsample2x(self, image):
+        """
+        Doubles the resolution of 'image'
+
+        Parameters
+        ----------
+        image: tensor
+            Image to upsample
+
+        Returns
+        -------
+        tensor
+            The upsampled image
+        """
         return F.interpolate(image, scale_factor=2, mode='bilinear')
 
-    def combine_images(self, image_a, image_b, alpha):
+    def _combine_images(self, image_a, image_b, alpha):
+        """
+        Combines 'image_a' and 'image_b' into one image, with the level
+        of influence of 'image_a' over the final image determined by 'alpha'
+
+        Parameters
+        ----------
+            image_a: tensor
+                The first image to be combined
+            image_b: tensor
+                The second image to be combined
+            alpha: float
+                The degree of influence of 'image_a' over the final image.
+                Goes from 0 to 1.
+
+        Returns
+        -------
+        tensor
+            The image resulting from combining the two images, given alpha
+        """
         return torch.lerp(image_a, image_b, alpha)
 
     @classmethod
-    def restoblock(cls, res):
+    def _restoblock(cls, res):
+        """
+        Returns the block id of the block responsible for handling the resolution 'res'
+
+        Parameters
+        ----------
+        res: int
+            An image resolution, it must be a power of 2
+
+        Returns
+        -------
+        int
+            The id (based on the parameter 'conv_blocks') of the block that
+            corresponds to the resolution 'res'
+
+        Raises
+        ------
+        AssertionError
+            If the image resolution is not a power of two
+        """
         log2res = log2(res)
         assert log2res.is_integer(), "stop_at_resolution should be a power of 2."
         block_id = int(log2res) - 2
         return block_id
 
     @classmethod
-    def blocktores(cls, block_id):
+    def _blocktores(cls, block_id):
+        """
+        Returns the resolution that corresponds to the block identified by 'block_id'
+
+        Parameters
+        ----------
+        block_id: int
+            The identifier of the block whose resolution is needed
+
+        Returns
+        -------
+        int
+            The resolution that corresponds to the block identified by 'block_id'
+            It is always a power of 2
+        """
         return 2**(block_id + 2)
 
-    def block_sequential_exec(self, w1, w2, alpha, stop_at_resolution, style_mixing):
-        stop_at_block = self.restoblock(stop_at_resolution)
+    def _block_sequential_exec(self, w1, w2, alpha, stop_at_resolution, style_mixing):
+        """
+        Executes the blocks in 'conv_blocks' in a sequential order thus growing an image
+        as the sequence progresses (progressive growing)
+
+        Parameters
+        ----------
+            w1: tensor
+                The first style tensor
+            w2: tensor
+                The second style tensor, used for style mixing
+            alpha: float
+                When combining two images, one being the result of a block and the
+                other being the result of an algorithmic upsample, it determines the
+                influence of one over the other
+            stop_at_resolution: int
+                The progressive growing stops at the resolution set by this parameter
+            style_mixing: bool
+                Controls wheter or not at some random point of the progressive growing
+                the second style tensor 'w2' replaces the first one 'w1'
+
+        Returns
+        -------
+        tensor
+            An image resulting from the progressive growing of the synthesis network.
+            Its resolution is dictated by 'stop_at_resolution'
+        """
+        stop_at_block = self._restoblock(stop_at_resolution)
         x = self.conv_blocks[0](w1)
         if stop_at_block == 0:
             return self.to_rgb[stop_at_block](x)
@@ -79,18 +213,71 @@ class SynthesisNetwork(nn.Module):
                 w = w2
             x = self.conv_blocks[i](x, w)
         image_to_upsample = self.to_rgb[stop_at_block - 1](x)
-        upsample_image = self.upsample2x(image_to_upsample)
+        upsample_image = self._upsample2x(image_to_upsample)
         conv_image = self.to_rgb[stop_at_block](self.conv_blocks[stop_at_block](x, w))
-        x = self.combine_images(upsample_image, conv_image, alpha)
+        x = self._combine_images(upsample_image, conv_image, alpha)
         return x
 
     def forward(self, w1, w2, alpha=1, stop_at_resolution=1024, style_mixing=False):
-        return self.block_sequential_exec(w1, w2, alpha, stop_at_resolution, style_mixing)
+        """
+        Public method that wraps the '_block_sequential_exec' private method
+
+        Parameters
+        ----------
+        w1: tensor
+            The first style tensor
+        w2: tensor
+            The second style tensor, used for style mixing
+        alpha: float, optional
+            When combining two images, one being the result of a block and the
+            other being the result of an algorithmic upsample, it determines the
+            influence of one over the other (default is 1)
+        stop_at_resolution: int, optional
+            The progressive growing stops at the resolution set by this parameter
+            (default is 1024)
+        style_mixing: bool, optional
+            Controls wheter or not at some random point of the progressive growing
+            the second style tensor 'w2' replaces the first one 'w1' (default is False)
+
+        Returns
+        -------
+        tensor
+            An image resulting from the progressive growing of the synthesis network.
+            Its resolution is dictated by 'stop_at_resolution'
+        """
+        return self._block_sequential_exec(w1, w2, alpha, stop_at_resolution, style_mixing)
 
 
 class StyleGAN(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(StyleGAN, self).__init__(*args, **kwargs)
+    """
+    Generator of the StyleGAN architecture.
+    Generates an output image out of a set of random z tensors
+
+    Attributes
+    ----------
+    mappn: MappingNetwork
+        Instance of the MappingNetowrk class. It is the first sub-network of this network
+    synthn: SynthesisNetwork
+        Instance of the SynthesisNetwork class. It's the second sub-network of this network
+    prob_style_mixing: float
+        The probability that the style mixing trick will take effect during a fordward
+        pass of the network
+    stop_at_resolution: int
+        The resolution of the resulting image tensor
+    alpha: float
+        The alpha step of the training, if the instance of the network is not being trained,
+        the value should be 1
+
+    Methods
+    -------
+    forward(z1, z2=None)
+        Returns an output image tensor generated by the network
+
+    blocktores(block_id)
+        Returns the resolution that corresponds to the block identified by 'block_id'
+    """
+    def __init__(self):
+        super(StyleGAN, self).__init__()
         self.mappn = MappingNetwork()
         self.synthn = SynthesisNetwork()
         self.prob_style_mixing = 0.3
@@ -98,6 +285,20 @@ class StyleGAN(nn.Module):
         self.alpha = 1
 
     def forward(self, z1, z2=None):
+        """
+        Parameters
+        __________
+        z1: tensor
+            A random tensor used as a seed to produce a tensor image
+        z2: tensor, optional
+            A random tensor used as a seed to produce a tensor image.
+            If its None then it won't be used and the style mixing effect won't
+            take place. (default is None)
+        Returns
+        -------
+        tensor
+            Returns an output image tensor generated by the network
+        """
         w1 = self.mappn(z1)
         if z2 is not None:
             w2 = self.mappn(z2)
@@ -110,4 +311,18 @@ class StyleGAN(nn.Module):
 
     @classmethod
     def blocktores(cls, block_id):
+        """
+        Returns the resolution that corresponds to the block identified by 'block_id'
+
+        Parameters
+        ----------
+        block_id: int
+            The identifier of the block whose resolution is needed
+
+        Returns
+        -------
+        int
+            The resolution that corresponds to the block identified by 'block_id'
+            It is always a power of 2
+        """
         return SynthesisNetwork.blocktores(block_id)
